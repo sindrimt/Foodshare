7b78fb1cb241631d44b7155f1224e3b43b12566b
sindrimt <sindrimt@stud.ntnu.no>
2022-02-08 23:23:12 +0100

    Functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 //import ReactDOM from "react-dom";
 
-export default function App() {
-  return <h1>Testing React halo mattias</h1>;
-}
+const App = () => {
+  return <h1>React connected til django! </h1>;
+};
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
+
+export default App;
