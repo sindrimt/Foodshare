2bb51e3180c374eefd98913135263f2a0bcd1f7d
Boye Gravningen Sjo <boyegs@stud.ntnu.no>
2022-02-15 15:38:59 +0100

    add image support

--- api/models.py
+++ api/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
+
+
+def upload_to(instance, filename):
+    return "recipes/{filename}".format(filename=filename)
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=30, unique=True)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        gettext_lazy("Image"),
+        upload_to=upload_to,
+        default="recipes/default.jpg",
+        blank=True,
+    )
+
     """ 
     author = models.ForeignKey
     category = ???
-    pic = ???
     etc.
     """
 
