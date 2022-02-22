d5093ae67f0a3db585f1e356651dd90122dcc9d9
sindrimt <sindrimt@stud.ntnu.no>
2022-02-22 12:18:06 +0100

    CardContainer to Functional Component
    CardContainer changed to a  Functional Component
    
    Related to issue: #7
    
    Individual programming:
    
    Sindri Mørch Tomasson <sinrimt@stud.ntnu.no>

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
-import CardContainer from "./CardContainer";
 import HomePage from "./HomePage";
-import Navbar from "./Navbar";
-
-//import ReactDOM from "react-dom";
 
 const App = () => {
   return (
     <>
-      {/* <Navbar /> */}
-      <HomePage/>
+      <HomePage />
     </>
   );
 };
-
-/*export default class App extends Component {
-  Constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return <h1>Welcome to the NEW FoodShare!</h1>;
-  }
-} */
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
--- frontend/src/components/CardContainer.js
+++ frontend/src/components/CardContainer.js
@@ -1,38 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RecipeCard from "./Card";
 
 import styled from "styled-components";
 import axios from "axios";
 
-class CardContainer extends React.Component {
-  url = "api/recipes";
-  state = {
-    recipes: [],
-  };
+const CardContainer = () => {
+  const url = "api/recipes";
+  const [state, setState] = useState([]);
 
-  componentDidMount() {
-    axios.get(this.url).then((res) => {
+  useEffect(() => {
+    axios.get(url).then((res) => {
       const recipes = res.data;
-      this.setState({ recipes });
-      console.log(this.state.recipes);
+      setState({ recipes });
     });
-  }
-  render() {
-    return (
-      <GridContainer>
-        {this.state.recipes.map((recipe) => (
-          <RecipeCard
-            key={recipe.id}
-            title={recipe.title}
-            description={recipe.content}
-            createdAt={recipe.created}
-            image={recipe.image}
-          />
-        ))}
-      </GridContainer>
-    );
-  }
-}
+  }, []);
+
+  return (
+    <GridContainer>
+      {state.recipes?.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          title={recipe.title}
+          description={recipe.content}
+          createdAt={recipe.created}
+          image={recipe.image}
+        />
+      ))}
+    </GridContainer>
+  );
+};
 
 const GridContainer = styled.section`
   display: grid;
