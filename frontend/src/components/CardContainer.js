17c5e9acb7b9aa90ffd19ddfdc419a0c44b2eb7b
sindrimt <sindrimt@stud.ntnu.no>
2022-02-24 14:44:22 +0100

    Basic search functionality added
    Searching now works, althoug very basic and with example data. This commit is related to #31 Induvidual programming: Sindri Mørch Tomasson <sindrimt@stud.ntnu.no>

--- frontend/src/components/CardContainer.js
+++ frontend/src/components/CardContainer.js
@@ -14,19 +14,78 @@ const CardContainer = () => {
       setState({ recipes });
     });
   }, []);
+  // ========
+
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const result = await axios.get(
+      "http://api.themoviedb.org/3/movie/popular?api_key=ad1ac1d7621db8c39803075fc3658091"
+    );
+    const dataList = result.data;
+    console.log(dataList.results);
+    setPopular(dataList.results);
+    setFiltered(dataList.results);
+  };
+
+  useEffect(() => {
+    const filter = popular?.filter((card) => {
+      return card.title.toLowerCase().includes(text);
+    });
+    setFiltered(filter);
+    console.log(filtered);
+  }, [text]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const filter = popular?.filter((card) => {
+      return card.title.toLowerCase().includes(text);
+    });
+    setFiltered(filter);
+    console.log(filtered);
+  };
 
   return (
-    <GridContainer>
-      {state.recipes?.map((recipe) => (
-        <RecipeCard
-          key={recipe.id}
-          title={recipe.title}
-          description={recipe.content}
-          createdAt={recipe.created}
-          image={recipe.image}
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="search"
+          onChange={(e) => {
+            setText(e.target.value.toLowerCase());
+          }}
         />
-      ))}
-    </GridContainer>
+      </form>
+      <GridContainer>
+        {state.recipes?.map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            title={recipe.title}
+            description={recipe.content}
+            createdAt={recipe.created}
+            image={recipe.image}
+          />
+        ))}
+
+        {filtered?.map((card) => {
+          return (
+            <RecipeCard
+              key={card.id}
+              title={card.title}
+              image={"https://image.tmdb.org/t/p/w500" + card.backdrop_path}
+            />
+          );
+        })}
+      </GridContainer>
+    </>
   );
 };
 
