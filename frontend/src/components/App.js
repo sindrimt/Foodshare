abae297e257d66d5287bcac30152891057aa254c
sindrimt <sindrimt@stud.ntnu.no>
2022-02-25 19:01:33 +0100

    Check if user is logged in
    This commit is related to #27
    
    Individual programming:
    Sindri Mørch Tomasson <sindrimt@stud.ntnu.no

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    // Create context => lagre om man er logget inn i state (true eller false)
+    //TODO Husk å set setLoggedIn til å være false når man logger ut
+    axios
+      .get("api/accounts/profile/")
+      .then((response) => {
+        console.log("Du er logget inn!");
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        console.log("Du er IKKE logget inn");
+      });
+  }, []);
+
   return (
     <>
       <HomePage />
