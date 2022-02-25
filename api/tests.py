fad48aadecc1bfefffd9dc14491476c78186cce0
Mattias <mattiald@stud.ntnu.no>
2022-02-25 16:26:16 +0100

    🧪 test(Testing): Testing modules
    Established fundation tests for modules
    
    Related to: #35
    Indivdual programming: Mattias Lund Dahlin <mattiald@stud.ntnu.no>

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Recipe
 
 # Create your tests here.
+
+class recipeTest(TestCase):
+
+    def create_recipe(self, title="test title", content="test content"):
+        return Recipe.objects.create(title=title, content=content)
+
+    def test_recipe_creation(self):
+        testObject=self.create_recipe()
+        self.assertTrue(isinstance(testObject, Recipe))
+
+    def test_recipe_title(self):
+        testObject=self.create_recipe()
+        self.assertEquals(testObject.title, "test title")
