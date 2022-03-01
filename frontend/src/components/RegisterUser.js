0b28e5dae9b2706125110bd7469928f6fb69dbf7
sindrimt <sindrimt@stud.ntnu.no>
2022-03-01 15:20:22 +0100

    + Register page fix
    Latest updates from Development branch
    
    Related to #40
    
    Individual programming:
    Sindri Mørch Tomasson <sindrimt@stud.ntnu.no>

--- frontend/src/components/RegisterUser.js
+++ frontend/src/components/RegisterUser.js
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import Radio from "@material-ui/core/Radio";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import Slider from "@material-ui/core/Slider";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 
