366129a39473dce9ede3aee46950ee9800f0e405
sindrimt <sindrimt@stud.ntnu.no>
2022-03-07 18:31:37 +0100

    Filter works, but...
    The filter function is working, but when pressing "all", nothing shows up

--- frontend/src/components/CardContainer.js
+++ frontend/src/components/CardContainer.js
@@ -1,40 +1,50 @@
 import React, { useState, useEffect } from "react";
-import RecipeCard from "./Card";
 import TestCard from "./TestCard";
-
 import styled from "styled-components";
 import axios from "axios";
 import { motion, AnimatePresence } from "framer-motion";
 import { AnimateSharedLayout } from "framer-motion";
-//import { useDebounce } from "use-debounce";
 import { useDebounce } from "use-debounce";
 import { TextField } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Stack from "@mui/material/Stack";
 
 const CardContainer = () => {
   const [popular, setPopular] = useState([]);
   const [filtered, setFiltered] = useState([]);
+  const [tagFilter, setTagFilter] = useState("");
+  const [tags, setTags] = useState([]);
   const [text, setText] = useState("");
   const [debouncedValue] = useDebounce(text, 300);
-
-  /*  useEffect(() => {
-    axios.get(url).then((res) => {
-      const recipes = res.data;
-      setState({ recipes });
-    });
-  }, []); */
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
+
+  const fetchTags = () => {
+    axios
+      .get("/api/tags/")
+      .then((res) => {
+        setTags(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const fetchData = async () => {
     const url = "api/recipes/";
     const result = await axios.get(url);
 
     const dataList = result.data;
-    console.log("==============");
-    console.log(dataList);
-    console.log("==============");
+
     setPopular(dataList);
     setFiltered(dataList);
   };
@@ -50,29 +60,53 @@ const CardContainer = () => {
     }
   }, [text]);
 
-  // Filters the movies based on title
+  // Filters the recipes on title and on tagname
   const filterFunction = () => {
     const filter = popular?.filter((card) => {
-      return card.title.toLowerCase().includes(text);
+      return (
+        card.title.toLowerCase().includes(text) && card.tags.includes(tagFilter)
+      );
     });
     setFiltered(filter);
-    //console.log(filtered);
   };
 
   // When you press enter on search (kind of useless)
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    const filter = popular?.filter((card) => {
-      return card.title.toLowerCase().includes(text);
-    });
-    setFiltered(filter);
-    //console.log(filtered);
+    filterFunction();
   };
+
+  const handleChange = (event) => {
+    setTagFilter(event.target.value);
+  };
+  /* Kjører hver gang tag blir oppdatert */
+  /* Fins sikkert en bedre måte å gjøre det på, men idc :P */
+  useEffect(() => {
+    filterFunction();
+  }, [tagFilter]);
 
   return (
     <>
       <FormContainer>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-autowidth-label">
+            Filter
+          </InputLabel>
+          <Select value={tagFilter} onChange={handleChange} label="Tags">
+            {/* <Stack spacing={1}> */}
+            {/* Stack Fungerer tydeligvis ikke for some reason >:( */}
+
+            <MenuItem value="">All</MenuItem>
+
+            {/* Rendrer hver tag i dropdownen */}
+            {tags?.map((tag) => (
+              <MenuItem key={tag.id} value={tag.name}>
+                {tag.name}
+              </MenuItem>
+            ))}
+            {/* </Stack> */}
+          </Select>
+        </FormControl>
         <form onSubmit={handleSubmit}>
           <TextField
             onChange={(e) => setText(e.target.value)}
