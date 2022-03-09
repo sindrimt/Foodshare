52f7507d94c45e773c0d7b092b6e3d1ade722a05
Boye Gravningen Sjo <boyegs@stud.ntnu.no>
2022-03-09 20:34:45 +0100

    clean profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -4,6 +4,10 @@ import Stack from "@mui/material/Stack";
 import Typography from "@material-ui/core/Typography";
 import RecipeGrid from "../components/RecipeGrid";
 import { UserContext } from "../context/UserContext";
+import EditIcon from "@mui/icons-material/Edit";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
 const ProfilePage = () => {
   const [posts, setPosts] = useState([]);
@@ -20,13 +24,35 @@ const ProfilePage = () => {
   }, [currentUser]);
 
   return (
-    <Stack spacing={2} alignItems="center">
-      <Typography variant="h3">
-        {currentUser.first_name + currentUser.last_name + "'s recipes"}
-      </Typography>
-      <Typography variant="h5">{"@" + currentUser.username}</Typography>
-      <RecipeGrid posts={posts} />
-    </Stack>
+    <>
+      <Stack spacing={2} alignItems="center">
+        <Typography variant="h3">
+          {currentUser.first_name + currentUser.last_name + "'s recipes"}
+        </Typography>
+        <Typography variant="h5">{"@" + currentUser.username}</Typography>
+        <Stack spacing={2} direction="row">
+          <Button
+            color="primary"
+            variant="contained"
+            to="/profile"
+            component={Link}
+          >
+            <EditIcon />
+            Edit profile
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            to="/profile/liked-recipes"
+            component={Link}
+          >
+            <ThumbUpIcon />
+            Liked recipes
+          </Button>
+        </Stack>
+        <RecipeGrid posts={posts} />
+      </Stack>
+    </>
   );
 };
 
