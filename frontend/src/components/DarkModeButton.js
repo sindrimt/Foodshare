a80fdaaf9b53b902e70910b612532b997add3665
Boye Gravningen Sjo <boyegs@stud.ntnu.no>
2022-03-17 17:45:28 +0100

    persistent theme storage

--- frontend/src/components/DarkModeButton.js
+++ frontend/src/components/DarkModeButton.js
@@ -3,24 +3,44 @@ import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { useTheme } from "@mui/material/styles";
 import { ColorModeContext } from "../context/ColorModeContext";
-import { Button } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 
 export default function DarkModeButton() {
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
   return (
-    <Button
-      variant="contained"
-      onClick={colorMode.toggleColorMode}
-      startIcon={
-        theme.palette.mode === "dark" ? (
+    <Tooltip
+      title={`Switch to ${
+        theme.palette.mode === "light" ? "dark" : "light"
+      } mode`}
+    >
+      {/* <Button
+        variant="contained"
+
+        startIcon={
+          theme.palette.mode === "dark" ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )
+        }
+      ></Button> */}
+      <IconButton
+        onClick={() => {
+          colorMode.toggleColorMode();
+          localStorage.setItem(
+            "mode",
+            theme.palette.mode === "light" ? "dark" : "light"
+          );
+        }}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
           <Brightness7Icon />
         ) : (
           <Brightness4Icon />
-        )
-      }
-    >
-      Switch theme
-    </Button>
+        )}
+      </IconButton>
+    </Tooltip>
   );
 }
