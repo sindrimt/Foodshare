ce74f2522b267c2f961870ff25bf2711d618ccce
erik2501 <eriksw2501@gmail.com>
2022-03-22 15:06:28 +0100

    🎉 init(RegisterUser & UserCreated): User feedback
    User feedback for not logged in action attemps

--- frontend/src/components/UserCreated.js
+++ frontend/src/components/UserCreated.js
@@ -8,12 +8,22 @@ const UserCreated = () => {
   return (
     <Grid container spacing={1}>
       <Grid item xs={12} align="center">
-        <Typography component="h2" variant="h2" id="title">
-          User succesfully created!
+      <br />
+      <br />
+        <Typography component="h3" variant="h3" id="title">
+          Bruker opprettet!
         </Typography>
         <br />
-        <Button color="primary" variant="contained" to="/" component={Link}>
-          OK
+        <br />
+        <br />
+        <br />
+        <Button color="primary" variant="outlined" to="/login" component={Link}>
+          Ta meg til login!
+        </Button>
+        <br />
+        <br />
+        <Button color="primary" variant="outlined" to="/" component={Link}>
+          Ta meg til oppskriftene!
         </Button>
       </Grid>
     </Grid>
