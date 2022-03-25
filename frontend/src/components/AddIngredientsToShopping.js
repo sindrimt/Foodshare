da03b5f056497584418480983e7e6f2c8ed2d2ba
Boye Gravningen Sjo <boyegs@stud.ntnu.no>
2022-03-25 14:59:15 +0100

    minor changes to add to cart button

--- frontend/src/components/AddIngredientsToShopping.js
+++ frontend/src/components/AddIngredientsToShopping.js
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import { Rating } from "@mui/material";
 import API from "../axios";
 
 const AddIngredientsToShopping = (props) => {
-  
-  
-
- 
   const handleAdd = () => {
     API.post(`/recipes/${props.id.toString()}/add_to_cart/`);
   };
 
-  
-
   return (
     <div>
-      <Button variant="info" color="red" onClick={handleAdd}>
-        Legg til i handleliste
+      <Button variant="contained" color="secondary" onClick={handleAdd}>
+        Add to shopping list
       </Button>
-      
     </div>
   );
 };
