18c80fa66c611f0bec6b6659dfdb8e8b0db52092
Boye Gravningen Sjo <boyegs@stud.ntnu.no>
2022-03-25 14:51:25 +0100

    add check if logged in on followed page Pair programming: Hedda Skåre <heddsk@stud.ntnu.no>

--- frontend/src/pages/BrowseFollowedPage.js
+++ frontend/src/pages/BrowseFollowedPage.js
@@ -1,24 +1,33 @@
 import Stack from "@mui/material/Stack";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import API from "../axios";
 import RecipeGrid from "../components/RecipeGrid";
 import FollowedUsersList from "../components/FollowedUsersList";
+import { UserContext } from "../context/UserContext";
+import { Typography } from "@mui/material";
 
 export default function BrowseFollowedPage() {
   const [posts, setPosts] = useState([]);
+  const { isLoggedIn } = useContext(UserContext);
 
   useEffect(() => {
-    API.get("recipes/by_followed/").then((response) => {
-      setPosts(response.data);
-    });
+    if (isLoggedIn) {
+      API.get("recipes/by_followed/").then((response) => {
+        setPosts(response.data);
+      });
+    }
   }, []);
 
   return (
     <>
-      <Stack spacing={2} alignItems="center">
-        <FollowedUsersList />
-        <RecipeGrid posts={posts} />
-      </Stack>
+      {isLoggedIn ? (
+        <Stack spacing={2} alignItems="center">
+          <FollowedUsersList />
+          <RecipeGrid posts={posts} />
+        </Stack>
+      ) : (
+        <Typography> You are not logged in. </Typography>
+      )}
     </>
   );
 }
