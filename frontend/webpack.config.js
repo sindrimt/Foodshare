747c230fbed27cf7c085c25c983e2c81bbe0adfc
sindrimt <sindrimt@gmail.com>
2022-04-25 21:19:43 +0200

    run

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,12 +30,17 @@ module.exports = {
       },
     }),
   ],
-  /*  plugins: [
+  plugins: [
     new webpack.DefinePlugin({
       "process.env": {
         // This has effect on the react lib size
         NODE_ENV: JSON.stringify("production"), //NODE_ENV: JSON.stringify("production"),
       },
     }),
-  ], */
+  ],
+  /*  output: {
+    filename: "build.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  }, */
 };
